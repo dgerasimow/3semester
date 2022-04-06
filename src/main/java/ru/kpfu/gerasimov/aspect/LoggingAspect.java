fa8f1449a5e7150d68fe5a347f882ef4e1b5d87e
dgerasimow <danilgerasimovsteam@gmail.com>
2022-04-06 20:15:19 +0300

    done task 6april

---
+++ src/main/java/ru/kpfu/gerasimov/aspect/LoggingAspect.java
@@ -0,0 +1,37 @@
+package ru.kpfu.gerasimov.aspect;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+import ru.kpfu.gerasimov.dto.UserDto;
+
+import java.util.Optional;
+
+
+@Component
+@Aspect
+public class LoggingAspect {
+
+    public static final Logger LOGGER = LoggerFactory.getLogger(LoggingAspect.class);
+
+    @Pointcut("@annotation(Loggable)")
+    public void logCityRequest() {
+
+    }
+
+    @After("logCityRequest()")
+    public Object logAllMethods(ProceedingJoinPoint proceedingJoinPoint) throws Throwable {
+        Optional<String> city = (Optional<String>) proceedingJoinPoint.getArgs()[0];
+
+        Object result = proceedingJoinPoint.proceed();
+
+        LOGGER.info("City request: {}", city.get());
+
+        return result;
+    }
+}
